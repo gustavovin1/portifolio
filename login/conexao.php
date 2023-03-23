d2daedc44998a67affe6801e1142e148093066d1
gustavovin1 <90657747+gustavovin1@users.noreply.github.com>
2023-03-23 20:19:14 -0300

    php 1.0

--- login/conexao.php
+++ login/conexao.php
@@ -1,11 +1,9 @@
 <?php
-    define('HOST', '127.0.0.1');
-    define('USUARIO', 'root');
-    define('SENHA', 'root');
-    define('DB', 'login');
+    // define('HOST', '127.0.0.1');
+    // define('USUARIO', 'root');
+    // define('SENHA', 'root');
+    // define('DB', 'login');
 
-    $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
-    $query = 'SELECT * FROM usuario';
-    $result = $conexao->query($query);
-    print_r($result);
+    // $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
+    $pdo = new PDO('mysql:localhost;dbname=login','root','root');
 ?>
--- login/index.php
+++ login/index.php
@@ -1,3 +1,16 @@
+<?php
+    $pdo = new PDO('mysql:host=localhost;dbname=login','root','root');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+//insert
+if(isset($_POST['usuario'])){
+    $sql = $pdo->prepare("INSERT INTO usuario(usuario, senha) VALUES(?,?)");
+    $sql->execute(array($_POST['usuario'], $_POST['senha']));
+    echo '<div class="pop-insert">inserido com sucesso</div>';
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,12 +21,13 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <form action="" method="POST" class="login-container">
+<div class="pop-insert">inserido com sucesso</div>
+    <form method="post" class="login-container">
         <img src="usuario-de-perfil.png" alt="">
         <input type="email" name="usuario" required placeholder="dude@email.com" id="email">
         <input type="password" name="senha" required placeholder="senha" id="password">
         <span><input type="checkbox" required name="check" id="check">concordar com os <a href="">termos de serviço</a></span>
-        <input type="button" value="Login">
+        <input type="submit" value="Login">
     </form>
 </body>
 </html>
