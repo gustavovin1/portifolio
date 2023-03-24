62264ecec6b6833ecaf3b33426efb0207d16af47
gustavovin1 <90657747+gustavovin1@users.noreply.github.com>
2023-03-23 23:05:02 -0300

    -a

---
+++ login/registros.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registros</title>
+    <link rel="stylesheet" href="registros.css">
+</head>
+<body>
+    <table>
+        <tr id="head">
+        <th>id</th>
+        <th>Usuario</th>
+        <th>Senha</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>gustavo</td>
+            <td>123</td>
+        </tr>
+    </table>
+</body>
+</html>
